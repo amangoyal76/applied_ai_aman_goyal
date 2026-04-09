d3a8980c41684d6153b2812aa72ca816c438bf48
amangoyal76 <135976462+amangoyal76@users.noreply.github.com>
2026-04-09 20:52:53 +0530

    Add files via upload

---
+++ Assignment2_DBSCAN/dbscan.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cluster import DBSCAN
+from sklearn.cluster import KMeans
+
+
+# Dataset load
+data = pd.read_csv("network_data.csv")
+
+print("Dataset Loaded Successfully")
+print(data.head())
+
+
+# Features select
+X = data[['packet_size', 'request_frequency']]
+
+
+# DBSCAN apply
+dbscan = DBSCAN(eps=500, min_samples=2)
+labels = dbscan.fit_predict(X)
+
+print("DBSCAN Labels:", labels)
+
+
+# DBSCAN plot
+plt.scatter(X.iloc[:, 0], X.iloc[:, 1], c=labels, cmap='rainbow')
+
+plt.title("DBSCAN Clustering")
+plt.xlabel("Packet Size")
+plt.ylabel("Request Frequency")
+
+plt.savefig("dbscan_plot.png")
+plt.show()
+
+
+# KMeans comparison
+kmeans = KMeans(n_clusters=2, random_state=42)
+kmeans_labels = kmeans.fit_predict(X)
+
+
+plt.scatter(X.iloc[:, 0], X.iloc[:, 1], c=kmeans_labels, cmap='viridis')
+
+plt.title("KMeans Clustering Comparison")
+plt.xlabel("Packet Size")
+plt.ylabel("Request Frequency")
+
+plt.savefig("kmeans_comparison_plot.png")
+plt.show()
